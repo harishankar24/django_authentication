81841a6fca366bdb5f28c21b9c385a9a043e6a75
Hari <hari@shankar.com>
2023-04-22 12:20:46 +0530

    Displayed Name,Email,Last Login at on user dashboard. This is version_2.

--- userss/views.py
+++ userss/views.py
@@ -5,35 +5,49 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
+def SignupView(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        username = request.POST.get('username')
+        email = request.POST['email']
+        password = request.POST['password']
+        # password = request.POST.get('password')
+        print(firstname, lastname, username, email, password )
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.first_name = firstname
+        user.last_name = lastname
+        user.save()
+        return HttpResponse("✔✔ User has been created successfully!! ")
+    return render(request, 'signup.html')
+
+
 def LoginView(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        my_user = authenticate(request, username=username, password=password)
-        if my_user is not None:
-            login(request, my_user)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
             return redirect('home')
         else:
             return HttpResponse("☠ Either Username or Password is incorrect. ☠")
     return render(request, 'login.html')
 
 
-def SignupView(request):
-    if request.method == 'POST':
-        full_name = request.POST.get('fullname')
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        print(full_name, username, email, password )
-        my_user = User.objects.create_user(username=username, email=email, password=password)
-        my_user.save()
-        return HttpResponse("✔✔ User has been created successfully!! ")
-    return render(request, 'signup.html')
-
-
 @login_required(login_url='login')
 def HomePageView(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        # print('Name: ', request.user.get_full_name())
+        # print('Last Login: ', request.user.last_login)
+        # print('Password: ', request.user.password)
+        # print('Email: ', request.user.email)
+        data = {
+            'name': request.user.get_full_name(),
+            'email': request.user.email,
+            'last_login': request.user.last_login,
+        }
+    return render(request, 'home.html', context=data)
 
 
 def LogoutView(request):
